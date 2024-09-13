7be8681909d29f6ad7a30ba8c552d4370811d346
evtravnikova <elena@travnikova.pro>
2024-09-13 19:19:22 +0200

    English version of site done.

---
+++ js/app.js
@@ -0,0 +1,12 @@
+/*
+let userLang = navigator.language; //"uk-UA"
+let urls = {
+    'uk-UA': 'http://localhost:63342/agromet/index.html',
+    'en': 'http://localhost:63342/agromet/index-en.html',
+};
+let defaultUrl = 'http://localhost:63342/agromet/index.html';
+
+let url = urls[userLang] ?? defaultUrl;
+console.log(url);
+console.log(navigator);
+*/
